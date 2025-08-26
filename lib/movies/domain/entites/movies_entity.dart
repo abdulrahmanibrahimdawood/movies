fc432630628dae37f5cbcfe45642fad28f34e668
Abdulrahman Ibrahim <abdoibrahim9111@gmail.com>
2025-08-27 01:31:55 +0300

    create movies entity

---
+++ lib/movies/domain/entites/movies_entity.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+
+class MoviesEntity extends Equatable {
+  final int id;
+  final String title;
+  final String backdropPath;
+  final List<int> genreIds;
+  final String overview;
+  final double voteAverage;
+
+  const MoviesEntity({
+    required this.id,
+    required this.title,
+    required this.backdropPath,
+    required this.genreIds,
+    required this.overview,
+    required this.voteAverage,
+  });
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => [
+    id,
+    title,
+    backdropPath,
+    genreIds,
+    overview,
+    voteAverage,
+  ];
+}
