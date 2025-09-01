7d7e89e74f24d6537f82519b05b659580107f6f7
Abdulrahman Ibrahim <abdoibrahim9111@gmail.com>
2025-09-01 16:06:36 +0300

    create UI & dummy

---
+++ lib/core/utils/recommendation_dummy.dart
@@ -0,0 +1,70 @@
+import 'package:movies/movies/domain/entites/genres_entity.dart';
+import 'package:movies/movies/domain/entites/movie_details_entity.dart';
+import 'package:movies/movies/domain/entites/recommendation.dart';
+
+MovieDetailsEntity movieDetailDummy = MovieDetailsEntity(
+  backdropPath: "/nmGWzTLMXy9x7mKd8NKPLmHtWGa.jpg",
+  genres: [GenresEntity(name: 'Family', id: 10751)],
+  id: 438148,
+  overview:
+      "A fanboy of a supervillain supergroup known as the Vicious 6, Gru hatches a plan to become evil enough to join them, with the backup of his followers, the Minions.",
+  releaseDate: "2022-06-29",
+  runtime: 87,
+  title: "Minions: The Rise of Gru",
+  voteAverage: 7.8,
+);
+
+List<RecommendationEntity> recommendationDummy = [
+  const RecommendationEntity(
+    backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg",
+    id: 924482,
+  ),
+  const RecommendationEntity(
+    backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg",
+    id: 924482,
+  ),
+  const RecommendationEntity(
+    backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg",
+    id: 924482,
+  ),
+  const RecommendationEntity(
+    backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg",
+    id: 924482,
+  ),
+  const RecommendationEntity(
+    backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg",
+    id: 924482,
+  ),
+  const RecommendationEntity(
+    backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg",
+    id: 924482,
+  ),
+  const RecommendationEntity(
+    backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg",
+    id: 924482,
+  ),
+  const RecommendationEntity(
+    backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg",
+    id: 924482,
+  ),
+  const RecommendationEntity(
+    backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg",
+    id: 924482,
+  ),
+  const RecommendationEntity(
+    backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg",
+    id: 924482,
+  ),
+  const RecommendationEntity(
+    backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg",
+    id: 924482,
+  ),
+  const RecommendationEntity(
+    backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg",
+    id: 924482,
+  ),
+  const RecommendationEntity(
+    backdropPath: "/ta17TltHGdZ5PZz6oUD3N5BRurb.jpg",
+    id: 924482,
+  ),
+];
